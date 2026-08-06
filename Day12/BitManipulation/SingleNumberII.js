ae008a7442744d03f48b27f0c1753279fd96b32f
Adarshanshu <adarshmpvm@gmail.com>
2026-08-07 01:44:54 +0530

    find two single numbers in the array

---
+++ Day12/BitManipulation/SingleNumberII.js
@@ -0,0 +1,49 @@
+
+// 260. Single Number III
+
+// Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+
+// You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,1,3,2,5]
+// Output: [3,5]
+// Explanation:  [5, 3] is also a valid answer.
+
+
+// Example 2:
+
+// Input: nums = [-1,0]
+// Output: [-1,0]
+
+
+// Example 3:
+
+// Input: nums = [0,1]
+// Output: [1,0]
+
+
+let find = (nums)=>{
+    let xor = 0;
+    for(let i=0;i<nums.length;i++){
+        xor ^= nums[i];
+    }
+    let diffBit = xor & -xor;
+    
+    let a=0;
+    let b=0;
+    for(let i=0;i<nums.length;i++){
+        if((nums[i]& diffBit)!==0){
+            a ^= nums[i];
+        }
+        else{
+            b ^= nums[i];
+        }
+    }
+    return [a,b];
+}
+
+console.log(find([1,2,1,3,2,5]))
