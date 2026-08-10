e27102829683eecdcbb75817cdac277a6db09cc0
Adarshanshu <adarshmpvm@gmail.com>
2026-08-10 23:42:07 +0530

    initial commit

---
+++ Day13/HashMap/ContainsDuplicateII.js
@@ -0,0 +1,41 @@
+
+// 219. Contains Duplicate II
+
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+
+
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+
+
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+
+let find = (nums,k)=>{
+    let map = {};
+    for(let i=0;i<nums.length;i++){
+        if(map[nums[i]]!==undefined){
+            if(map[nums[i]]<=k){
+                return true;
+            }
+        }
+        map[nums[i]] = i;
+    }
+    return false;
+};
+
+
+let nums = [1,2,3,1];
+let k= 3;
+console.log(find(nums,k));
